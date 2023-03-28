8527c88122314fd3bfdf8e0a951ec49d5098efe2
Chukwuka Bright <101369922+Brytchukz@users.noreply.github.com>
2023-03-28 16:02:40 +0100

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""
-Island Perimeter:
-    returns the perimeter of the island described in grid
+"""This module contains the function island_perimeter problem solution
 """
 
 
 def island_perimeter(grid):
-    """island perimenter function"""
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
+    """This function returns the perimeter of the island described in grid
+    """
+
+    if not grid:
+        return 0  # empty grid has no perimeter
+    perimeter = 0  # perimeter of the island
+    for row in range(len(grid)):  # rows in grid
+        for col in range(len(grid[row])):  # columns in grid
+            """ if the cell is land (1) then check the perimeter of the cell
+                and add it to the perimeter of the island described in grid
+            """
+            if grid[row][col] == 1:
                 perimeter += 4
-                if i > 0 and grid[i-1][j] == 1:
+                # check top cell if exists
+                if row > 0 and grid[row - 1][col] == 1:
+                    # subtract 2 from perimeter if top cell is land
                     perimeter -= 2
-                if j > 0 and grid[i][j-1] == 1:
+                    # check left cell if exists
+                if col > 0 and grid[row][col - 1] == 1:
+                    # subtract 2 from perimeter if left cell is land
                     perimeter -= 2
-    return 
+    return perimeter  # return perimeter of the island
